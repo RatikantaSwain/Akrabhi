f5d57d838da5d5c30d4a26c6c81fd94d85f538eb
RatikantaSwain <swaindevdutt@gmail.com>
2023-11-08 16:01:37 +0530

    modified test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ let text=document.querySelector(".text");
 
 function add(){
     if(inputs.value==""){
-        alert("please enter");
+        alert("please enter your name: ");
     }
     else{
         let a=document.creatElement("ul");
